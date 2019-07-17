f0f4182c83c471659a6a76229e42de52265a1ffa
MuriloDutra <murilo.dutra2000@gmail.com>
2019-07-16 22:21:44 -0300

    Implementando funcionalidade de salar fotos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -8,23 +8,11 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 })
 
 export class Tab2Page {
-
   currentImage: any;
-  constructor(private camera: Camera) {}
 
-  takePicture(){
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
+  constructor(public photoService: PhotoService) { }
 
-    this.camera.getPicture(options).then((imageData) => {
-      this.currentImage = 'data: image/jpeg;base64,' + imageData;
-    }, (err) => {
-      //Handle error
-      console.log("Camera issue: " + err);
-    });
+  ngOnItit(){
+    this.photoService.loadSaved();
   }
 }
